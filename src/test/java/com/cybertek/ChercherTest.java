a1ed5af1c2e46d6dfc5075249e265a121fea4bd4
MikeJ77 <mikejohnson3505@gmail.com>
2022-01-02 22:10:59 +0300

    Last coomit related to this class

--- src/test/java/com/cybertek/ChercherTest.java
+++ src/test/java/com/cybertek/ChercherTest.java
@@ -1,7 +1,14 @@
 package com.cybertek;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -10,6 +17,8 @@ import java.util.concurrent.TimeUnit;
 
 public class ChercherTest {
 WebDriver driver;
+WebDriverWait wait;
+
 
     @BeforeMethod
     public void setUp() {
@@ -27,8 +36,28 @@ WebDriver driver;
     @Test
 
     public void alertPresentTest() {
+        WebElement initiateAlert=driver.findElement(By.id("alert"));
+        initiateAlert.click();
 
+        wait=new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.alertIsPresent());
 
+        Alert alert=driver.switchTo().alert();
+        alert.accept();
     }
 
+    @Test
+    public void disabledButtonTest(){
+
+        WebElement button=driver.findElement(By.id("disable"));
+        System.out.println("button.isEnabled() = " + button.isEnabled());
+
+        WebElement buttonIniater= driver.findElement(By.id("enable-button"));
+        buttonIniater.click();
+
+        wait=new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.elementToBeClickable(button));
+        System.out.println("button.isEnabled() = " + button.isEnabled());
+        Assert.assertTrue(button.isEnabled(), "verify the button is enabled");
+    }
 }
